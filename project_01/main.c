68133d8394c69f8f05ef2fb4bb61eeff170dcb52
Andrey Kuharev <10.kuharev@gmail.com>
2021-04-06 00:09:50 +0300

    Version 3.0(working with files)

--- project_01/main.c
+++ project_01/main.c
@@ -14,146 +14,153 @@ int main(int argc, char *agrv[]) {
 	int size;
 //
 	do {
+		FILE *input, *output;
+		input = fopen("input.txt", "r");
+		output = fopen("output.txt", "w");
+
 //Блок 3: выбираем режим работы калькулятора
 		printf(
 				"Выберите режим калькулятора (c - работа с числами, v - работа с векторами): ");
-		scanf("%s", &choose);
+		fscanf(input, "%s", &choose);
 //
 		switch (choose) {
 		case 'c':
 			printf("Активирован режим работы с числами\n");
 // Блок 4: вводим первое число и арифметическую операцию. Так же вводим второе число/степень, если того требует операция
 			printf("Введите первое число: ");
-			scanf("%f", &a);
+			fscanf(input, "%f", &a);
 			printf(
 					"Введите одну из доступных операций (+,-,*,/,!,#(корень),^): ");
-			scanf(" %c", &oper);
+			fscanf(input, " %c", &oper);
 			if (oper == '+' || oper == '-' || oper == '*' || oper == '/') {
 				printf("Введите второе число: ");
-				scanf("%f", &b);
+				fscanf(input, "%f", &b);
 			} else if (oper == '^') {
 				printf("Введите степень, в которую хотите возвести число: ");
-				scanf("%f", &step);
+				fscanf(input, "%f", &step);
 			}
 //
 // Блок 5: вычисления в режиме работы с числами
 			switch (oper) {
 			case '+':
 				res = a + b;
-				printf("%f + %f = %f\n", a, b, res);
+				fprintf(output, "%f + %f = %f\n", a, b, res);
 				break;
 			case '-':
 				res = a - b;
-				printf("%f - %f = %f\n", a, b, res);
+				fprintf(output, "%f - %f = %f\n", a, b, res);
 				break;
 			case '*':
 				res = a * b;
-				printf("%f * %f = %f\n", a, b, res);
+				fprintf(output, "%f * %f = %f\n", a, b, res);
 				break;
 			case '/':
 				if (b == 0) {
-					printf(
+					fprintf(output,
 							"Делить на 0 нельзя, введите второе число еще раз: ");
-					scanf("%f", &b);
+					fscanf(input, "%f", &b);
 					res = a / b;
-					printf("%f / %f = %f\n", a, b, res);
+					fprintf(output, "%f / %f = %f\n", a, b, res);
 				} else {
 					res = a / b;
-					printf("%f / %f = %f\n", a, b, res);
+					fprintf(output, "%f / %f = %f\n", a, b, res);
 				}
 				break;
 			case '!':
 				if (a == 0)
-					printf("Факториал числа %f = 1\n", a);
+					fprintf(output, "Факториал числа %f = 1\n", a);
 				else if (a == 1)
-					printf("Факториал числа %f = 1\n", a);
+					fprintf(output, "Факториал числа %f = 1\n", a);
 				else {
 					for (float i = 1; i <= a; i++) {
 						fak = fak * i;
 					}
-					printf("Факториал числа %f = %li\n", a, fak);
+					fprintf(output, "Факториал числа %f = %li\n", a, fak);
 				}
 				break;
 			case '^':
 				if (step == 0)
-					printf("1\n");
+					fprintf(output, "1\n");
 				else {
-					printf("%0.5f\n", pow(a, step));
+					fprintf(output, "%0.5f\n", pow(a, step));
 				}
 				break;
 			case '#':
 				if (a < 0)
-					printf("корень из отрицательного числа извлекать нельзя\n");
+					fprintf(output,
+							"корень из отрицательного числа извлекать нельзя\n");
 				else
-					printf("%.5f\n", sqrt(a));
+					fprintf(output, "%.5f\n", sqrt(a));
 				break;
 			default:
-				printf("Неизвестная операция\n");
+				fprintf(output, "Неизвестная операция\n");
 			}
 //
 		case 'v':
 			printf("Активирован режим работы с векторами\n");
 			printf("Введите размер векторов: ");
-			scanf("%i", &size);
+			fscanf(input, "%i", &size);
 			x = malloc(size * sizeof(float));
 			y = malloc(size * sizeof(float));
 			printf("x:\n");
 			for (int i = 0; i < size; i++)
-				scanf("%f", &x[i]);
+				fscanf(input, "%f", &x[i]);
 			printf("y:\n");
 			for (int i = 0; i < size; i++)
-				scanf("%f", &y[i]);
+				fscanf(input, "%f", &y[i]);
 			printf(
-					"Введите одну из доступных операций (+,-,*(скалярное произведение))");
-			scanf("%s", &operv);
+					"Введите одну из доступных операций (+,-,*(скалярное произведение)): ");
+			fscanf(input, "%s", &operv);
 			switch (operv)
 // Блок 6: вычисления в режиме работы с векторами
 			{
 			case '+':
-				printf("( ");
+				fprintf(output, "( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", x[i]);
-				printf(") + ( ");
+					fprintf(output, "%.2f ", x[i]);
+				fprintf(output, ") + ( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", y[i]);
-				printf(") = ( ");
+					fprintf(output, "%.2f ", y[i]);
+				fprintf(output, ") = ( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", x[i] + y[i]);
-				printf(")\n");
+					fprintf(output, "%.2f ", x[i] + y[i]);
+				fprintf(output, ")\n");
 				break;
 
 			case '-':
-				printf("( ");
+				fprintf(output, "( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", x[i]);
-				printf(") - ( ");
+					fprintf(output, "%.2f ", x[i]);
+				fprintf(output, ") - ( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", y[i]);
-				printf(") = ( ");
+					fprintf(output, "%.2f ", y[i]);
+				fprintf(output, ") = ( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", x[i] - y[i]);
-				printf(")\n");
+					fprintf(output, "%.2f ", x[i] - y[i]);
+				fprintf(output, ")\n");
 				break;
 			case '*':
-				printf("( ");
+				fprintf(output, "( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", x[i]);
-				printf(") * ( ");
+					fprintf(output, "%.2f ", x[i]);
+				fprintf(output, ") * ( ");
 				for (int i = 0; i < size; i++)
-					printf("%.2f ", y[i]);
-				printf(") = ");
+					fprintf(output, "%.2f ", y[i]);
+				fprintf(output, ") = ");
 				for (int i = 0; i < size; i++)
 					rez += x[i] * y[i];
-				printf("%.2f \n", rez);
+				fprintf(output, "%.2f \n", rez);
 				break;
 			default:
-				printf("Неизвестная операция\n");
+				printf( "Неизвестная операция\n");
 			}
 //
 // Блок 7: тут если мы вводим "y", программа стартует сначала, если "n", то программа прекращает работу
 			printf("Желаете продолжить? (y/n): ");
-			scanf("%s", &k);
+			fscanf(input, "%s", &k);
 		}
+		fclose(input);
+		fclose(output);
 	} while (k == 'y');
 //
 	return 0;
